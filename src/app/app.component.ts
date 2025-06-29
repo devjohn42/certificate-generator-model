c714d85a468bc73d8113972dd6363edc32f822dc
devjohn42 <devjohncb@gmail.com>
2025-06-29 16:18:59 -0300

    :sparkles: feat: page - first state of the page listing the certificates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import { CertifiedItemComponent } from "./_components/certified-item/certified-i
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
+import { CertificatesComponent } from "./pages/certificates/certificates.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertifiedItemComponent, BaseUiComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertifiedItemComponent, BaseUiComponent, CertificatesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
