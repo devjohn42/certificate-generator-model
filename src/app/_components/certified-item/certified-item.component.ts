67299eeae00d07913010b00be958f995cf6580b5
devjohn42 <devjohncb@gmail.com>
2025-06-29 15:43:31 -0300

    :sparkles: feat: component - certificate list item

---
+++ src/app/_components/certified-item/certified-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { SecondaryButtonComponent } from "../secondary-button/secondary-button.component";
+
+@Component({
+  selector: 'app-certified-item',
+  imports: [SecondaryButtonComponent],
+  templateUrl: './certified-item.component.html',
+  styleUrl: './certified-item.component.css'
+})
+export class CertifiedItemComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CertifiedItemComponent } from "./_components/certified-item/certified-item.component";
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { PrimaryButtonComponent } from "./_components/primary-button/primary-button.component";
 import { SecondaryButtonComponent } from "./_components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-root',
-  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent],
+  imports: [NavbarComponent, PrimaryButtonComponent, SecondaryButtonComponent, CertifiedItemComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
