66acd4fc27607c80f0be1f75a2a73e9011cb2a5e
devjohn42 <devjohncb@gmail.com>
2025-07-03 22:12:02 -0300

    :sparkles: feat: component - list of certificates

--- src/app/pages/certificates/certificates.component.ts
+++ src/app/pages/certificates/certificates.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { CertifiedItemComponent } from "../../_components/certified-item/certified-item.component";
 import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-certificates',
-  imports: [SecondaryButtonComponent],
+  imports: [SecondaryButtonComponent, CertifiedItemComponent],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
