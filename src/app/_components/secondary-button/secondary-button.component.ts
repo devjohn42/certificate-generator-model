1dd312064a56d6cb591a6a72aa44ddc304b0e0cc
devjohn42 <devjohncb@gmail.com>
2025-08-15 22:56:14 -0300

    :sparkles: feat: creates inputs for buttons

--- src/app/_components/secondary-button/secondary-button.component.ts
+++ src/app/_components/secondary-button/secondary-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-secondary-button',
@@ -7,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrl: './secondary-button.component.css'
 })
 export class SecondaryButtonComponent {
-
+  @Input() textoBotao: string = ''
+  @Input() phClass: string = ''
 }
--- src/app/pages/certificates/certificates.component.ts
+++ src/app/pages/certificates/certificates.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { CertifiedItemComponent } from "../../_components/certified-item/certified-item.component";
+import { SecondaryButtonComponent } from "../../_components/secondary-button/secondary-button.component";
 
 @Component({
   selector: 'app-certificates',
-  imports: [CertifiedItemComponent],
+  imports: [CertifiedItemComponent, SecondaryButtonComponent, RouterLink],
   templateUrl: './certificates.component.html',
   styleUrl: './certificates.component.css'
 })
